74975fcf5c38b714abaf6baba45d8e229c43ac72
vlantonakos <anvlachavas@gmail.com>
2024-08-30 23:30:27 +0300

    user + orders

---
+++ backend/microservices-example/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public Mono<String> getUser() {
+        return Mono.just("User details");
+    }
+}
